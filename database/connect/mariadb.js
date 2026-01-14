684c78bf99c57b8eecf72de9e23010e27f12c55e
jhleejh98-boop <jhleejh98@gmail.com>
2026-01-14 14:34:10 +0900

    mariadb.js commit

--- database/connect/mariadb.js
+++ database/connect/mariadb.js
@@ -2,7 +2,7 @@ const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
   host: 'localhost',
-  port: 3307,
+  port: 3306,
   user: 'root',
   password: '5461',
   database: 'portfolio',
